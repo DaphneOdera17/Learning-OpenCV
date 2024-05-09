c493aaefbe56ffea9644967a60d25aec43df5325
DaphneOdera17 <2944006086@qq.com>
2024-05-09 13:25:45 +0800

    add drawMoments module

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -49,5 +49,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/video.dir/DependInfo.cmake"
+  "CMakeFiles/homework1.dir/DependInfo.cmake"
   )
--- homework1.cpp
+++ homework1.cpp
@@ -3,10 +3,77 @@
 using namespace cv;
 using namespace std;
 
+Mat img;
+
+void drawMoments(vector<Point> contours)
+{
+    Moments M;
+    M = moments(contours);
+    double cX = double(M.m10 / M.m00);
+    double cY = double(M.m01 / M.m00);
+
+    circle(img, Point2d(cX, cY), 1, Scalar(0, 255, 0), 2, 8);
+    putText(img, "center", Point2d(cX - 20, cY - 20), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 1, 8);
+    cout << cX << " " << cY << endl;
+}
+
+void getContours(Mat imgDil, Mat img)
+{
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+
+    findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+
+    for(int i = 0; i < contours.size(); i ++)
+    {
+        int area = contourArea(contours[i]);
+        cout << area << endl;
+
+        vector<vector<Point>> conPoly(contours.size());
+        vector<Rect> boundRect(contours.size());
+        string objectType;
+
+        if(area > 1000) 
+        {
+            float peri = arcLength(contours[i], true);
+
+            approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+
+            drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
+
+            drawMoments(contours[i]);
+
+            cout << conPoly[i].size() << endl;
+
+            boundRect[i] = boundingRect(conPoly[i]);
+            rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 5);
+
+            int objCor = (int)conPoly[i].size();
+
+            if(objCor == 3) {
+                objectType = "Tri";
+            }
+            else if(objCor == 4) {
+                // objectType = "Rect";
+                float aspRatio = (float)boundRect[i].width / (float)boundRect[i].height;
+                if(aspRatio > 0.95 && aspRatio < 1.05)
+                    objectType = "Square";
+                else
+                    objectType = "Rectangle";
+            }
+            else {
+                objectType = "Circle";
+            }
+
+            putText(img, objectType, {boundRect[i].x, boundRect[i].y - 5}, FONT_HERSHEY_DUPLEX, 0.75, Scalar(0, 69, 255), 2);
+        }
+    }
+}
+
 int main()
 {
     string path = "../image/color.jpg";
-    Mat img = imread(path, IMREAD_COLOR);
+    img = imread(path, IMREAD_COLOR);
 
     vector<Mat> channels;
     split(img, channels);
@@ -20,6 +87,19 @@ int main()
     subtract(binaryR, binaryG, subtracted);
     Mat result;
     result = subtracted;
+
+    Mat imgGray, imgBlur, imgCanny, imgDil, imgErode; 
+
+    // Preprocessing
+    cvtColor(img, imgGray, COLOR_BGR2GRAY);
+    GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
+    Canny(imgBlur, imgCanny, 25, 75);
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+    dilate(imgCanny, imgDil, kernel);
+
+    getContours(imgDil, img);
+
+    // drawMoments();
 
     imshow("Img", img);
     imshow("Binary Image", result);
